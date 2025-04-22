7716b5268122befdd42c83f0659e8afea8490bae
murrmaringo <twizzleannie@gmail.com>
2025-04-23 00:16:20 +0300

    4th

---
+++ src/main/java/org/example/CurrencyRateJob.java
@@ -0,0 +1,24 @@
+package org.example
+
+import jakarta.annotation.PostConstruct;
+import lombok.RequiredArgsConstructor;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+import org.example.CurrencyRateDto;
+import org.example.CbrClient;
+
+import java.util.List;
+
+@Component
+@RequiredArgsConstructor
+public class CurrencyRateJob {
+
+    private final CbrClient cbrClient;
+
+    @Scheduled(cron = "0 0 * * *")
+    @PostConstruct
+    public void fetchAndPrintRates() {
+        List<CurrencyRateDto> rates = cbrClient.fetchRates();
+        rates.forEach(System.out::println);
+    }
+}
